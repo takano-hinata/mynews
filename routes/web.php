0e419cf46ce659d0a28c43f8311ec1aa35cc8ed0
takano-hinata <nekoneko0108@yahoo.co.jp>
2020-09-20 16:20:50 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,17 @@ Route::get('XXX', 'AAAController@bbb');
 
 //課題４
 Route::group(['prefix' => 'admin'], function() {
-     Route::get('profile/create', 'Admin\ProfileController@add');
-     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
-Route::get('admin/news/create', 'Admin\NewsController@add');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
