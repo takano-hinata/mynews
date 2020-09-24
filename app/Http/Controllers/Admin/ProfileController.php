88825db3923113e671d65244beb85dcbdded5971
takano-hinata <nekoneko0108@yahoo.co.jp>
2020-09-24 07:16:26 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,25 +4,39 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
-    public function add() 
+    public function add()
     {
         return view('admin.profile.create');
     }
     
-    public function create() 
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        unset($form['_token']);
+        unset($form['image']);
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
     
-    public function edit() 
+    public function edit()
     {
         return view('admin.profile.edit');
     }
     
-    public function update() 
+    public function update()
     {
         return redirect('admin/profile/edit');
     }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -3,54 +3,55 @@
 @section('title', 'プロフィール作成画面')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
-                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
-                    
-                    @if (count($errors) > 0)
-                       <ul>
-                           @foreach($errors->all() as $e)
-                              <li>{{ $e }}</li
-                           @endforeach
-                       </ul>
-                    @endif
-                    <div class="form-group row">
-                        <lavel class="col-md-2">名前</lavel>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('title') }}" placeholder="氏名を入力">
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2">性別</label>
-                        <div class="col-md-10">
-                            <select class="form-control" name="gender">
-                                <option value="">性別を選択</option>
-                                <option value="男">男</option>
-                                <option value="女">女</option>
-                                <option value="その他">その他</option>
-                            </select>
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <lavel class="col-md-2">趣味</lavel>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="hobby" rows="5" placeholder="趣味を入力">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20" placeholder="自己紹介文を入力">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    
-                </form>
-            </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mx-auto">
+      <h2>My プロフィール</h2>
+      <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+        @if (count($errors) > 0)
+    　　　　　 <ul>
+        　　@foreach($errors->all() as $e)
+         　　　 <li>{{ $e }}</li>
+        　 　@endforeach
+        　 </ul>
+     　　@endif
+        <div class="form-group row">
+          <lavel class="col-md-2">名前</lavel>
+          <div class="col-md-10">
+            <input type="text" class="form-control" name="name" value="{{ old('title') }}" placeholder="氏名を入力">
+          </div>
         </div>
+
+        <div class="form-group row">
+          <label class="col-md-2">性別</label>
+          <div class="col-md-10">
+            <select class="form-control" name="gender">
+              <option value="">性別を選択</option>
+              <option value="男">男</option>
+              <option value="女">女</option>
+              <option value="その他">その他</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <lavel class="col-md-2">趣味</lavel>
+          <div class="col-md-10">
+            <textarea class="form-control" name="hobby" rows="5" placeholder="趣味を入力">{{ old('body') }}</textarea>
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label class="col-md-2">自己紹介欄</label>
+          <div class="col-md-10">
+            <textarea class="form-control" name="introduction" rows="20" placeholder="自己紹介文を入力">{{ old('body') }}</textarea>
+          </div>
+        </div>
+        {{ csrf_field() }}
+        　　　　　　　　　　　 <input type="submit" class="btn btn-primary" value="更新">
+      </form>
     </div>
+  </div>
+</div>
 @endsection
